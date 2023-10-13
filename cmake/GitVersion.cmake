3be15a090723856219d969ea15dbd705802bfa33
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2023-10-13 12:46:09 +0200

    Makes Git dynamic versionning work.

--- cmake/GitVersion.cmake
+++ cmake/GitVersion.cmake
@@ -11,7 +11,7 @@ function(git_version)
 		PROJECT_PREFIX #
 		PROJECT_SOURCE #
 		FALLBACK_VERSION #
-		OUTPUT_VERSION
+		OUTPUT_VERSION #
 	)
 
 	set(ARGS_MultiValue "")
@@ -38,6 +38,10 @@ function(git_version)
 		set(ARGS_OUTPUT_VERSION PROJECT_VERSION)
 	endif(NOT ARGS_OUTPUT_VERSION)
 
+	if(NOT ARGS_OUTPUT_VERSION)
+		set(ARGS_OUTPUT_VERSION PROJECT_FULL_VERSION)
+	endif(NOT ARGS_OUTPUT_VERSION)
+
 	set(GitVersion_ARGS #
 		PROJECT_SOURCE ${ARGS_PROJECT_SOURCE} #
 		VERSION_FILE ${CMAKE_CURRENT_BINARY_DIR}/.version
@@ -60,7 +64,10 @@ function(git_version)
 
 	set(version_file ${CMAKE_CURRENT_BINARY_DIR}/.version)
 
-	file(READ ${version_file} describe_version)
+	file(READ ${version_file} version_file_content)
+	string(REPLACE " " ";" version_file_list ${version_file_content})
+	list(GET version_file_list 0 base_version)
+	list(GET version_file_list 1 full_version)
 
 	add_custom_target(
 		${ARGS_PROJECT_PREFIX}Version ALL
@@ -80,7 +87,12 @@ function(git_version)
 	)
 
 	set(${ARGS_OUTPUT_VERSION}
-		${describe_version}
+		${base_version}
+		PARENT_SCOPE
+	)
+
+	set(${ARGS_PROJECT_PREFIX}FULL_VERSION
+		${full_version}
 		PARENT_SCOPE
 	)
 
@@ -91,7 +103,7 @@ function(_git_version_from_git_describe)
 	list(APPEND ARGS_Options ENSURE)
 	set(ARGS_OneValue)
 
-	list(APPEND ARGS_OneValue PROJECT_SOURCE OUTPUT_VERSION)
+	list(APPEND ARGS_OneValue PROJECT_SOURCE OUTPUT_VERSION OUTPUT_FULL_VERSION)
 
 	set(ARGS_MultiValue "")
 
@@ -117,7 +129,7 @@ function(_git_version_from_git_describe)
 	find_package(Git REQUIRED)
 
 	execute_process(
-		COMMAND ${GIT_EXECUTABLE} describe --tags --dirty --match=?[0-9.]*
+		COMMAND ${GIT_EXECUTABLE} describe --tags --dirty
 		WORKING_DIRECTORY ${ARGS_PROJECT_SOURCE}
 		OUTPUT_VARIABLE describe_name
 		OUTPUT_STRIP_TRAILING_WHITESPACE
@@ -146,6 +158,12 @@ function(_git_version_from_git_describe)
 		${CMAKE_MATCH_2}
 		PARENT_SCOPE
 	)
+
+	set(${ARGS_OUTPUT_FULL_VERSION}
+		${CMAKE_MATCH_1}
+		PARENT_SCOPE
+	)
+
 endfunction(_git_version_from_git_describe)
 
 function(_git_version_run)
@@ -187,18 +205,28 @@ function(_git_version_run)
 		set(git_describe_args ENSURE)
 	endif(ARGS_FALLBACK_VERSION)
 	_git_version_from_git_describe(
-		PROJECT_SOURCE ${ARGS_PROJECT_SOURCE} #
-		OUTPUT_VERSION describe_version #
+		PROJECT_SOURCE
+		${ARGS_PROJECT_SOURCE} #
+		OUTPUT_VERSION
+		describe_version #
+		OUTPUT_FULL_VERSION
+		describe_full_version
 		${git_describe_args}
 	)
+
 	if(EXISTS ${ARGS_VERSION_FILE})
 		file(READ ${ARGS_VERSION_FILE} version_file_content)
-		if(version_file_content EQUAL describe_version)
+
+		if(version_file_content STREQUAL
+		   "${describe_version} ${describe_full_version}"
+		)
 			return()
 		endif()
 	endif(EXISTS ${ARGS_VERSION_FILE})
 
-	file(WRITE ${ARGS_VERSION_FILE} ${describe_version})
+	file(WRITE ${ARGS_VERSION_FILE}
+		 "${describe_version} ${describe_full_version}"
+	)
 
 endfunction(_git_version_run)
 
